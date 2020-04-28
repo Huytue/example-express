094769f3e90bde6dfeedda75713b4fa9a79211af
Huytue <bachvonam1996@gmail.com>
2020-04-28 21:59:27 +0700

    cookie

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 var userRoutes = require('./routes/user.route');
+var cookieParser = require('cookie-parser');
 const port = 3000;
 const app = express();
 app.set("view engine", "pug");
@@ -9,6 +10,7 @@ app.set("views", "./views");
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(express.static('public'));
+app.use(cookieParser());
 app.get("/", function (req, res) {
   res.render("index", {
     name: "Alia",
