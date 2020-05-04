daf3081770b9c99814ca6ac7b17e22e1b3c869f6
Huytue <bachvonam1996@gmail.com>
2020-05-04 15:47:47 +0700

    CSRF Attack

--- db.js
+++ db.js
@@ -2,5 +2,9 @@ const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
 const adapter = new FileSync("db.json");
 const db = low(adapter);
-db.defaults({ users: [] , sessions: []}).write();
+db.defaults({ 
+	users: [], 
+	sessions: [],
+	transfers:[]
+}).write();
 module.exports = db;
--- index.js
+++ index.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 var cookieParser = require('cookie-parser');
+var csurf = require('csurf');
 
 var userRoutes = require('./routes/user.route');
 var authRoutes = require('./routes/auth.route');
 var productRoutes = require('./routes/product.route');
 var cartRoutes = require('./routes/cart.route');
+var transferRoutes = require('./routes/transfer.route');
 
 var authMiddleware = require("./middlewares/auth.middleware");
 var sessionMiddleware = require("./middlewares/session.middleware");
@@ -21,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 app.use(express.static('public'));
 app.use(cookieParser(process.env.SESSION_SECRET));
 app.use(sessionMiddleware);
+app.use(csurf({ cookie: true }));
 
 app.get("/", function (req, res) {
   res.render("index", {
@@ -32,6 +35,7 @@ app.use('/users',authMiddleware.requireAuth, userRoutes);
 app.use('/auth', authRoutes);
 app.use('/products', productRoutes);
 app.use('/cart', cartRoutes);
+app.use('/transfer', authMiddleware.requireAuth, transferRoutes);
 
 app.listen(port, function () {
   console.log("Server listing on port", port);
